6a0f5727c3a1d84228dad64a4936eaff28710f07
hatetavian <tavianjdawson123@gmail.com>
2022-01-19 14:03:01 -0600

    creeated login/sign up page

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -61,12 +61,15 @@ router.post('/', (req, res) => {
         username: req.body.username,
         email: req.body.email,
         password: req.body.password
+    }).then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
     })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
 });
 
 router.post('/login', (req, res) => {
@@ -88,10 +91,28 @@ router.post('/login', (req, res) => {
         res.status(400).json({ message: "Incorrect Password!" });
         return
     }
-     res.json({ user: dbUserData, message: 'You are now logged in!' });
+
+    req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+        
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
 
   });  
-})
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+          res.status(204).end();
+        });
+      }
+      else {
+        res.status(404).end();
+      }
+});
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
